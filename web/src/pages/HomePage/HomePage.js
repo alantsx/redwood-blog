38fb1c05094261f21a36a627e49ac139bec5d474
Alan <alanpfabricio@gmail.com>
2022-01-13 15:34:27 -0300

    postscell

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,14 +1,12 @@
 import { MetaTags } from '@redwoodjs/web'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
 
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.js</code>
-      </p>
+      <BlogPostsCell />
     </>
   )
 }
